08b36d9eafdffdac90c804d6d9eaf2bd7a1a0057
Bryon Volmer <bryoninvegas@gmail.com>
2018-02-05 09:16:35 -0800

    Chapt 8 additional game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and Running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("Up and Running");
+  var cards = ["queen", "queen", "king", "king"];
+  var cardsInPlay = [].push(cardOne);
+  var cardOne = [0];
+console.log("user flipped queen");
 
-console.log("User flipped " + cardOne);
+var cardTwo =[2];
+  var cardsInPlay = [].push(cardTwo);
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  alert("you found a match");
+  } else {
+  alert("sorry, try again.");
+}
