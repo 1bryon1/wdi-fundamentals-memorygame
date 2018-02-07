b7a9f0c654272138bd09f7c832cd8dddd6e40fe4
Bryon Volmer <bryoninvegas@gmail.com>
2018-02-07 10:00:56 -0800

    early Chap 10 game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,34 @@
 console.log("Up and Running");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardId = 0;
-var cardsInPlay = [].push(cards[cardId]);
+var cardsInPlay = [].push(cards[cardId].rank);
 
 console.log("User flipped a queen");
 console.log("user flipped a king");
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
 //start card match
 var checkForMatch = function () {
@@ -20,7 +43,7 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 var flipCard = function(cardId){
 var flipCard = (0);
 var flipCard = (2);
-console.log("A" + cards[cardId] + "was flipped!");
+console.log("A" + cards[cardId].rank + "was flipped!");
 if (cardsInPlay.length === 2 && cardsInplay[0] === cardsInPlay[1]) {
   alert ("You Found A Match!");
 }else{
