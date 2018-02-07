c81b4e85d1af70f449e634027d32a2006c5cea81
Bryon Volmer <bryoninvegas@gmail.com>
2018-02-06 20:08:54 -0800

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,23 @@
 console.log("Up and Running");
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay.push(cards[cardId]);
 
-var flipCard = function(cardId)[
+var cards = ["queen", "queen", "king", "king"];
+var cardId = 0;
+var cardsInPlay = [].push(cards[cardId]);
+
+console.log("User flipped a queen");
+console.log("user flipped a king");
+
+//start card match
+var checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("you found a match");
+  } else {
+  console.log("sorry, try again.");
+  }
+}
+
+//start card flip
+var flipCard = function(cardId){
 var flipCard = (0);
 var flipCard = (2);
 console.log("A" + cards[cardId] + "was flipped!");
@@ -10,14 +25,5 @@ if (cardsInPlay.length === 2 && cardsInplay[0] === cardsInPlay[1]) {
   alert ("You Found A Match!");
 }else{
   alert ("Sorry Try Again");
-  }
-}
-
-
-var checkForMatch = function () {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("you found a match");
-  } else {
-  console.log("sorry, try again.");
   }
 }
