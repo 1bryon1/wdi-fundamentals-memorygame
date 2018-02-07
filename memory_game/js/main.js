b46bc75a3739274ddae7154d748ddbd7b2524153
Bryon Volmer <bryoninvegas@gmail.com>
2018-02-06 16:25:16 -0800

    updated .js to Chap 8

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 console.log("Up and Running");
-  var cards = ["queen", "queen", "king", "king"];
-  var cardsInPlay = [].push(cardOne);
-  var cardOne = [0];
-console.log("user flipped queen");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay.push(cards[cardId]);
 
-var cardTwo =[2];
-  var cardsInPlay = [].push(cardTwo);
-console.log("user flipped king");
+var flipCard = function(cardId)[
+var flipCard = (0);
+var flipCard = (2);
+console.log("A" + cards[cardId] + "was flipped!");
+if (cardsInPlay.length === 2 && cardsInplay[0] === cardsInPlay[1]) {
+  alert ("You Found A Match!");
+}else{
+  alert ("Sorry Try Again");
+  }
+}
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-  alert("you found a match");
+
+var checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("you found a match");
   } else {
-  alert("sorry, try again.");
+  console.log("sorry, try again.");
+  }
 }
